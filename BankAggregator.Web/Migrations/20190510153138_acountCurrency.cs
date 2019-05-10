036ff19d156ef3b22d8b7256b5e6fd2e98cf9fa4
nwikechisom <oge.nwike@gmail.com>
2019-05-10 16:53:42 +0100

    accountOnboarding

---
+++ BankAggregator.Web/Migrations/20190510153138_acountCurrency.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BankAggregator.Web.Migrations
+{
+    public partial class acountCurrency : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Currency",
+                table: "AccountModels",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Currency",
+                table: "AccountModels");
+        }
+    }
+}
