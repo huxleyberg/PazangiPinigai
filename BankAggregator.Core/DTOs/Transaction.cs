efd987d462905ab3d474aa4eea086e7f0a9dc103
“chidi.udeogu” <huxleyberg@live.com>
2019-05-11 03:06:36 +0100

    added transction model

---
+++ BankAggregator.Core/DTOs/Transaction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace BankAggregator.Core.DTOs
+{
+    public class Transaction
+    {
+        public string TransactionRef { get; set; }
+        public string AccountNumber { get; set; }
+        public string AccountName { get; set; }
+        public string TransactionType { get; set; }
+        public decimal Amount { get; set; }
+        public string Currency { get; set; }
+        public DateTime TransDate { get; set; }
+        public string SourceDestinationBank { get; set; }
+    }
+}
